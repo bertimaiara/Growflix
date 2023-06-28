d09dc5640ffd82dec14fea7933a687947598380a
Maiara Berti <mr.ricardoberti@gmail.com>
2023-06-28 10:14:28 -0300

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,169 @@
+const growcast = [
+  {
+    img: "https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg",
+    title: "Growcast #00 | Conheça a Growdev",
+    category: "Growcast",
+    link: "https://youtu.be/aJ-HZuLcKeA",
+  },
+  {
+    img: "https://img.youtube.com/vi/7OWT3lfHYvE/sddefault.jpg",
+    title: "Growcast #01 | As profissões do futuro",
+    category: "Growcast",
+    link: "https://youtu.be/7OWT3lfHYvE",
+  },
+  {
+    img: "https://img.youtube.com/vi/ci1eQGPK89o/sddefault.jpg",
+    title: "Growcast #02 | Como seguir a carreira internacional?",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=ci1eQGPK89o",
+  },
+  {
+    img: "https://img.youtube.com/vi/WM7pGG7OicQ/sddefault.jpg",
+    title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=WM7pGG7OicQ",
+  },
+  {
+    img: "https://img.youtube.com/vi/2c8VOBZ160Y/sddefault.jpg",
+    title: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=2c8VOBZ160Y",
+  },
+  {
+    img: "https://img.youtube.com/vi/mxnEm03cYPc/sddefault.jpg",
+    title:
+      "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=mxnEm03cYPc",
+  },
+  {
+    img: "https://img.youtube.com/vi/m7DyR07KrOE/sddefault.jpg",
+    title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=m7DyR07KrOE",
+  },
+  {
+    img: "https://img.youtube.com/vi/vW8G9KPwRL0/sddefault.jpg",
+    title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=vW8G9KPwRL0",
+  },
+  {
+    img: "https://img.youtube.com/vi/_Ut2KqAqPyk/sddefault.jpg",
+    title:
+      "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=_Ut2KqAqPyk",
+  },
+  {
+    img: "https://img.youtube.com/vi/dfDgs0Ive2Q/sddefault.jpg",
+    title:
+      "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+    category: "Growcast",
+    link: "https://www.youtube.com/watch?v=dfDgs0Ive2Q",
+  },
+];
+
+const webinar = [
+  {
+    img: "https://img.youtube.com/vi/WKbLXuP9S-c/sddefault.jpg",
+    title: "Webinar - Recriando a interface do Linkedin com Flutter",
+    category: "Webinar",
+    link: "https://www.youtube.com/watch?v=WKbLXuP9S-c",
+  },
+  {
+    img: "https://img.youtube.com/vi/dJ6y1zMjVXU/sddefault.jpg",
+    title: "Webinar - Recriando a interface do Instagram com Flutter",
+    category: "Webinar",
+    link: "https://www.youtube.com/watch?v=dJ6y1zMjVXU",
+  },
+  {
+    img: "https://img.youtube.com/vi/8rdJsqUjsGg/sddefault.jpg",
+    title: "Webinar - Recriando a interface do iFood com Flutter",
+    category: "Webinar",
+    link: "https://www.youtube.com/watch?v=8rdJsqUjsGg",
+  },
+  {
+    img: "https://img.youtube.com/vi/C4p_7qBsECw/sddefault.jpg",
+    title: "Webinar - Recriando a interface do PicPay com Flutter",
+    category: "Webinar",
+    link: "https://www.youtube.com/watch?v=C4p_7qBsECw",
+  },
+  {
+    img: "https://img.youtube.com/vi/L1pKoMC6hWk/sddefault.jpg",
+    title: "Webinar - Recriando o layout do WhatsApp com Flutter",
+    category: "Webinar",
+    link: "https://www.youtube.com/watch?v=L1pKoMC6hWk",
+  },
+];
+
+const uxUi = [
+  {
+    img: "https://img.youtube.com/vi/FiZMowbaiJA/sddefault.jpg",
+    title: "Live #5 Jornada UX/UI: O UX/UI Design é para todos?",
+    category: "ux/ui",
+    link: "https://www.youtube.com/watch?v=FiZMowbaiJA",
+  },
+  {
+    img: "https://img.youtube.com/vi/cwqDu0ZzbYE/sddefault.jpg",
+    title:
+      "Live #4 Jornada UX/UI: Como migrar para o UX/UI Design: a trajetória de uma designer - Growdev",
+    category: "ux/ui",
+    link: "https://www.youtube.com/watch?v=cwqDu0ZzbYE",
+  },
+  {
+    img: "https://img.youtube.com/vi/ZMZjQ9aRuIY/sddefault.jpg",
+    title:
+      "Live #3 Jornada UX/UI: As oportunidades de estudar um programa de formação pagando após formado",
+    category: "ux/ui",
+    link: "https://www.youtube.com/watch?v=ZMZjQ9aRuIY",
+  },
+  {
+    img: "https://img.youtube.com/vi/8sd3wuKTZMA/sddefault.jpg",
+    title:
+      "Live #2 Jornada UX/UI: O profissional de UX/UI Design no mercado digital - Growdev",
+    category: "ux/ui",
+    link: "https://www.youtube.com/watch?v=8sd3wuKTZMA",
+  },
+  {
+    img: "https://img.youtube.com/vi/-26aqPtgoEY/sddefault.jpg",
+    title:
+      "Live #1 Jornada UX/UI: O papel do UX/UI Designer no desenvolvimento de um produto digital - Growdev",
+    category: "ux/ui",
+    link: "https://www.youtube.com/watch?v=-26aqPtgoEY",
+  },
+];
+
+const diversos = [
+  {
+    img: "https://img.youtube.com/vi/QsuhYgsSYQo/sddefault.jpg",
+    title: "Célula de talentos - Uma solução Growdev",
+    category: "diversos",
+    link: "https://www.youtube.com/watch?v=QsuhYgsSYQo",
+  },
+  {
+    img: "https://img.youtube.com/vi/ouDn8z3PSqs/sddefault.jpg",
+    title: "Como é o dia a dia de um dev? Live Growdev com Marcelo Eltz",
+    category: "diversos",
+    link: "https://www.youtube.com/watch?v=ouDn8z3PSqs",
+  },
+  {
+    img: "https://img.youtube.com/vi/cQDQNX6NXYo/sddefault.jpg",
+    title:
+      "Onde trabalhar com qualidade de software? Conheça a Testing Company, empresa parceira da Growdev",
+    category: "diversos",
+    link: "https://www.youtube.com/watch?v=cQDQNX6NXYo",
+  },
+  {
+    img: "https://img.youtube.com/vi/nKGbolkirBM/sddefault.jpg",
+    title: "WEBINAR ANALISTA DE DADOS - com Paulo Marcon da Arezzo&Co",
+    category: "diversos",
+    link: "https://www.youtube.com/watch?v=nKGbolkirBM",
+  },
+  {
+    img: "https://img.youtube.com/vi/6sZPsqNjeek/sddefault.jpg",
+    title: "Tecnologia trazendo soluções para a Arezzo & CO | Ricardo Pinho",
+    category: "diversos",
+    link: "https://www.youtube.com/watch?v=6sZPsqNjeek",
+  },
+];
---
+++ js/home.js
@@ -0,0 +1,128 @@
+const growcastItems = document.getElementById("growcast-items");
+const webinarItems = document.getElementById("webinar-items");
+const uxuiItems = document.getElementById("ux-ui-items");
+const diversosItems = document.getElementById("diversos-items");
+
+function renderGrowcast() {
+    let html = '';
+    
+    growcast.forEach(item =>{
+        html += `
+        <div class="col-12 col-sm-6 col-md-3 col-movie">
+            <div onmouseenter="showDetail(this)" onmouseleave="closeDetail(this)">
+                <img src="${item.img}" class="img-fluid">
+                <p class="detail-movie" data-link="${item.link}" style="display: none;" onclick>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16"
+                        fill="currentColor"
+                        class="bi bi-play-circle"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                    </svg>
+                    <span>${item.title}</span>
+                </p>
+            </div>
+        </div>
+        `
+    });
+    growcastItems.innerHTML += html;
+}
+
+renderGrowcast();
+
+function renderWebinar() {
+    let html = '';
+    
+    webinar.forEach(item =>{
+        html += `
+        <div class="col-12 col-sm-6 col-md-3 col-movie">
+            <div onmouseenter="showDetail(this)" onmouseleave="closeDetail(this)">
+                <img src="${item.img}" class="img-fluid">
+                <p class="detail-movie" data-link="${item.link}" style="display: none;" onclick>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16"
+                        fill="currentColor"
+                        class="bi bi-play-circle"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                    </svg>
+                    <span>${item.title}</span>
+                </p>
+            </div>
+        </div>
+        `
+    });
+    webinarItems.innerHTML += html;
+}
+
+renderWebinar();
+
+function renderUxUi() {
+    let html = '';
+    
+    uxUi.forEach(item =>{
+        html += `
+        <div class="col-12 col-sm-6 col-md-3 col-movie">
+            <div onmouseenter="showDetail(this)" onmouseleave="closeDetail(this)">
+                <img src="${item.img}" class="img-fluid">
+                <p class="detail-movie" data-link="${item.link}" style="display: none;" onclick>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16"
+                        fill="currentColor"
+                        class="bi bi-play-circle"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                    </svg>
+                    <span>${item.title}</span>
+                </p>
+            </div>
+        </div>
+        `
+    });
+    uxuiItems.innerHTML += html;
+}
+
+renderUxUi();
+
+function renderDiversos() {
+    let html = '';
+    
+    diversos.forEach(item =>{
+        html += `
+        <div class="col-12 col-sm-6 col-md-3 col-movie">
+            <div onmouseenter="showDetail(this)" onmouseleave="closeDetail(this)">
+                <img src="${item.img}" class="img-fluid">
+                <p class="detail-movie" data-link="${item.link}" style="display: none;" onclick>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16"
+                        fill="currentColor"
+                        class="bi bi-play-circle"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                    </svg>
+                    <span>${item.title}</span>
+                </p>
+            </div>
+        </div>
+        `
+    });
+    diversosItems.innerHTML += html;
+}
+
+renderDiversos();
